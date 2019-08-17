5aef9c79c56067068fd1dcd5bfd0939f81d5ffb1
FlyawayChan <15088131426@163.com>
2019-08-18 01:31:28 +0800

    1、瀑布流布局练习

---
+++ RunToodemo01/pages/my/my.js
@@ -0,0 +1,91 @@
+// pages/my/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      imgData:[
+          {
+            "src":"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566073012814&di=68003fb0f6d11d698696dfdcd4fa306b&imgtype=0&src=http%3A%2F%2Fpic51.nipic.com%2Ffile%2F20141028%2F17549279_112226510170_2.jpg",
+            "no": 1
+          },
+          {
+            "src": "../../image/img02.jfif",
+            "no": 2
+          },
+          {
+            "src": "../../image/img03.jfif",
+            "no": 3
+          },
+          {
+            "src": "../../image/img01.jfif",
+            "no": 4
+          },
+          {
+            "src": "../../image/img02.jfif",
+            "no": 5
+          },
+          {
+            "src": "../../image/img03.jfif",
+            "no": 6
+          }
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
